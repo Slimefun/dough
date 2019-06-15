ce83a96286799de2469ef95d3f91b6903204c9cb
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-06-15 18:40:13 +0200

    Added "math" package

---
+++ src/io/github/thebusybiscuit/cscorelib2/math/DoubleHandler.java
@@ -0,0 +1,55 @@
+package io.github.thebusybiscuit.cscorelib2.math;
+
+import java.text.DecimalFormat;
+
+public final class DoubleHandler {
+	
+	private DoubleHandler() {};
+	
+	/**
+	 * This method returns a very fancy representation of Doubles.
+	 * 
+	 * 1000 		-> 1K
+	 * 32000		-> 32K
+	 * 232 000 000	-> 2.32M
+	 * (and so on)
+	 * 
+	 * @param d	The double that should be formatted
+	 * @return	A fancy representation of this double
+	 */
+	public static String getFancyDouble(double d) {
+		DecimalFormat format = new DecimalFormat("##.##");
+		
+		double d2 = d / 1000000000000000d;
+		if (d2 > 1.0) return format.format(d2).replace(",", ".") + "Q";
+		
+		d2 = d / 1000000000000d;
+		if (d2 > 1.0) return format.format(d2).replace(",", ".") + "T";
+		
+		d2 = d / 1000000000d;
+		if (d2 > 1.0) return format.format(d2).replace(",", ".") + "B";
+		
+		d2 = d / 1000000d;
+		if (d2 > 1.0) return format.format(d2).replace(",", ".") + "M";
+		
+		d2 = d / 1000d;
+		if (d2 > 1.0) return format.format(d2).replace(",", ".") + "K";
+		
+		return format.format(d).replace(",", ".");
+	}
+	
+	public static double fixDouble(double amount, int digits) {
+		if (digits == 0) return (int) amount;
+		StringBuilder format = new StringBuilder("##");
+		for (int i = 0; i < digits; i++) {
+			if (i == 0) format.append(".");
+			format.append("#");
+		}
+		return Double.valueOf(new DecimalFormat(format.toString()).format(amount).replace(",", "."));
+	}
+	
+	public static double fixDouble(double amount) {
+		return fixDouble(amount, 2);
+	}
+
+}
