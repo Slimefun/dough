3360f20ead72b64b984edc6a19e9f95e6deb3a5f
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-06-16 12:28:43 +0200

    Added Updater Interface

--- src/io/github/thebusybiscuit/cscorelib2/updater/BukkitUpdater.java
+++ src/io/github/thebusybiscuit/cscorelib2/updater/BukkitUpdater.java
@@ -18,9 +18,10 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import lombok.Getter;
 import lombok.Setter;
 
-public class BukkitUpdater {
+public class BukkitUpdater implements Updater {
 	
 	private static final String api_url = "https://api.curseforge.com/servermods/files?projectIds=";
 	private static final char[] blacklist = "abcdefghijklmnopqrstuvwxyz-+_ ()[]{}".toCharArray();
@@ -33,6 +34,7 @@ public class BukkitUpdater {
 	private URL download;
 	private File file;
 	
+	@Getter
 	private String localVersion;
 	private String remoteVersion;
 	
@@ -57,16 +59,16 @@ public class BukkitUpdater {
 			for (int i = 0; i < remoteSplit.length; i++) {
 				if ((localSplit.length - 1) < i) {
 					return true;
-	        		}
+	        	}
 				
 				if (Integer.parseInt(localSplit[i]) > Integer.parseInt(remoteSplit[i])) {
-	        			return false;
-	        		}
-				
-	        		if (Integer.parseInt(remoteSplit[i]) > Integer.parseInt(localSplit[i])) {
-	        			return true;
-	        		}
+	        		return false;
 	        	}
+				
+	        	if (Integer.parseInt(remoteSplit[i]) > Integer.parseInt(localSplit[i])) {
+	        		return true;
+	        	}
+	        }
 	        
 	        	return false;
 		};
@@ -75,8 +77,8 @@ public class BukkitUpdater {
 		for (String dev: development_builds) {
 			if (localVersion.contains(dev)) {
 				System.err.println(" ");
-                		System.err.println("################## - DEVELOPMENT BUILD - ##################");
-                		System.err.println("You appear to be using an experimental build of " + plugin.getName());
+				System.err.println("################## - DEVELOPMENT BUILD - ##################");
+                System.err.println("You appear to be using an experimental build of " + plugin.getName());
 				System.err.println("Version " + localVersion);
 				System.err.println(" ");
 				System.err.println("Auto-Updates have been disabled. Use at your own risk!");
@@ -88,11 +90,12 @@ public class BukkitUpdater {
 		localVersion = localVersion.toLowerCase();
 		
 		// Deleting all unwanted characters
-        	for (char blocked: blacklist) {
+        for (char blocked: blacklist) {
 			localVersion = localVersion.replace(String.valueOf(blocked), "");
-        	}
+        }
 	}
 	
+	@Override
 	public void start() {
 		try {
 			this.url = new URL(api_url + id);
@@ -115,8 +118,8 @@ public class BukkitUpdater {
 					check();
 				} 
 				catch(NumberFormatException x) {
-		        		System.err.println(" ");
-		        		System.err.println("#################### - ERROR - ####################");
+					System.err.println(" ");
+		        	System.err.println("#################### - ERROR - ####################");
 					System.err.println("Could not auto-update " + plugin.getName());
 					System.err.println("Unrecognized Version: \"" + localVersion + "\"");
 					System.err.println("#################### - ERROR - ####################");
@@ -136,6 +139,7 @@ public class BukkitUpdater {
 			    final JsonArray array = (JsonArray) new JsonParser().parse(reader.readLine());
 			    if (array.size() == 0) {
 				    System.err.println("[CS-CoreLib - Updater] Could not connect to BukkitDev for Plugin \"" + plugin.getName() + "\", is it down?");
+				    
 				    try {
 					    thread.join();
 				    } catch (InterruptedException x) {
@@ -152,7 +156,7 @@ public class BukkitUpdater {
 			    remoteVersion = remoteVersion.toLowerCase();
 
 			    for (char blocked: blacklist) {
-				remoteVersion = remoteVersion.replace(String.valueOf(blocked), "");
+			    	remoteVersion = remoteVersion.replace(String.valueOf(blocked), "");
 			    }
 
 			    return true;
@@ -165,7 +169,7 @@ public class BukkitUpdater {
 				}
 			    	return false;
 			}
-	    	}
+	    }
 		
 		private void check() {
 			if (predicate.hasUpdate(localVersion, remoteVersion)) {
@@ -192,16 +196,16 @@ public class BukkitUpdater {
 	        	connection = (HttpURLConnection) url.openConnection();
 
 	        	connection.setInstanceFollowRedirects(false);
-	            	connection.setConnectTimeout(5000);
-	            	connection.addRequestProperty("User-Agent", "Auto Updater (by mrCookieSlime)");
+	            connection.setConnectTimeout(5000);
+	            connection.addRequestProperty("User-Agent", "Auto Updater (by mrCookieSlime)");
 
-	            	switch (connection.getResponseCode()) {
-	                	case HttpURLConnection.HTTP_MOVED_PERM:
-	               		case HttpURLConnection.HTTP_MOVED_TEMP:
-	                    		String loc = connection.getHeaderField("Location");
-	                    		location = new URL(new URL(location), loc).toExternalForm();
-	                    		continue;
-	            	}
+	            switch (connection.getResponseCode()) {
+	                case HttpURLConnection.HTTP_MOVED_PERM:
+	               	case HttpURLConnection.HTTP_MOVED_TEMP:
+	                    	String loc = connection.getHeaderField("Location");
+	                    	location = new URL(new URL(location), loc).toExternalForm();
+	                    	continue;
+	            }
 			
 	            break;
 	        }
@@ -224,26 +228,26 @@ public class BukkitUpdater {
 				    final byte[] data = new byte[1024];
 				    int read;
 				    while ((read = input.read(data, 0, 1024)) != -1) {
-					output.write(data, 0, read);
+				    	output.write(data, 0, read);
 				    }
 				} catch (Exception ex) {
 					System.err.println(" ");
 					System.err.println("#################### - ERROR - ####################");
-						System.err.println("Could not auto-update " + plugin.getName());
-						System.err.println("#################### - ERROR - ####################");
-						System.err.println(" ");
-						ex.printStackTrace();
+					System.err.println("Could not auto-update " + plugin.getName());
+					System.err.println("#################### - ERROR - ####################");
+					System.err.println(" ");
+					ex.printStackTrace();
 				} finally {
 				    try {
-					if (input != null) input.close();
-					if (output != null) output.close();
-					System.err.println(" ");
-					System.err.println("#################### - UPDATE - ####################");
-					System.err.println(plugin.getName() + " was successfully updated (" + localVersion + " -> " + remoteVersion + ")");
-					System.err.println("Please restart your Server in order to use the new Version");
-					System.err.println(" ");
+						if (input != null) input.close();
+						if (output != null) output.close();
+						System.err.println(" ");
+						System.err.println("#################### - UPDATE - ####################");
+						System.err.println(plugin.getName() + " was successfully updated (" + localVersion + " -> " + remoteVersion + ")");
+						System.err.println("Please restart your Server in order to use the new Version");
+						System.err.println(" ");
 				    } catch (IOException e) {
-					e.printStackTrace();
+				    	e.printStackTrace();
 				    }
 				    try {
 					    thread.join();
---
+++ src/io/github/thebusybiscuit/cscorelib2/updater/Updater.java
@@ -0,0 +1,9 @@
+package io.github.thebusybiscuit.cscorelib2.updater;
+
+public interface Updater {
+	
+	String getLocalVersion();
+	
+	void start();
+	
+}
