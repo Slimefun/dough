b2167f0491c64c0695ff1c1a920e6e374f5c31ad
TheBusyBiscuit <TheBusyBiscuit@users.noreply.github.com>
2019-09-04 19:36:15 +0200

    Delete sonar.sh

--- sonar.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn compile sonar:sonar -Dsonar.login=${SONARCLOUD_TOKEN} -Dsonar.host.url=https://sonarcloud.io -Dsonar.projectKey=TheBusyBiscuit_CS-CoreLib2 -Dsonar.organization=thebusybiscuit-github
