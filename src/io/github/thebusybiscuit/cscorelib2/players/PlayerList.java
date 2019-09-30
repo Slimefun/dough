9001ca6fe8ad1b0e50f5552ccbf09ad33a4fc11a
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-10-01 01:25:02 +0200

    Minor change

--- src/io/github/thebusybiscuit/cscorelib2/players/PlayerList.java
+++ src/io/github/thebusybiscuit/cscorelib2/players/PlayerList.java
@@ -3,6 +3,7 @@ package io.github.thebusybiscuit.cscorelib2.players;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -11,17 +12,19 @@ public final class PlayerList {
 
 	private PlayerList() {}
 	
+	public static Stream<Player> stream() {
+		return Bukkit.getOnlinePlayers().stream().map(p -> (Player) p);
+	}
+	
 	public static Optional<Player> findByName(String name) {
-		return Bukkit.getOnlinePlayers().stream()
+		return stream()
 				.filter(p -> p.getName().equalsIgnoreCase(name))
-				.findAny()
-				.map(p -> (Player) p);
+				.findAny();
 	}
 	
 	public static Set<Player> findPermitted(String permission) {
-		return Bukkit.getOnlinePlayers().stream()
+		return stream()
 				.filter(p -> p.hasPermission(permission))
-				.map(p -> (Player) p)
 				.collect(Collectors.toSet());
 	}
 	
