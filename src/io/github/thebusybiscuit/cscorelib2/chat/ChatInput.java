20bf13dc937d6020e10e5956098f7e3bc1385d3f
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-10-01 01:55:54 +0200

    Added a Consumer variant to ChatInput.java

--- src/io/github/thebusybiscuit/cscorelib2/chat/ChatInput.java
+++ src/io/github/thebusybiscuit/cscorelib2/chat/ChatInput.java
@@ -1,6 +1,7 @@
 package io.github.thebusybiscuit.cscorelib2.chat;
 
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 import org.bukkit.entity.Player;
@@ -11,6 +12,27 @@ public final class ChatInput {
 	private static ChatInputListener listener;
 
 	private ChatInput() {}
+	
+	public static void waitForPlayer(Plugin plugin, Player p, Consumer<String> handler) {
+		waitForPlayer(plugin, p, s -> true, handler);
+	}
+	
+	public static void waitForPlayer(Plugin plugin, Player p, Predicate<String> predicate, Consumer<String> handler) {
+		if (listener == null) listener = new ChatInputListener(plugin);
+		listener.handlers.put(p.getUniqueId(), new IChatInput() {
+			
+			@Override
+			public boolean test(String msg) {
+				return predicate.test(msg);
+			}
+			
+			@Override
+			public void onChat(Player p, String msg) {
+				handler.accept(msg);
+			}
+			
+		});
+	}
 	
 	public static void waitForPlayer(Plugin plugin, Player p, BiConsumer<Player, String> handler) {
 		waitForPlayer(plugin, p, s -> true, handler);
