eeee8706b4d176d0eda5a536a13db2af7ca50246
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-10-19 13:25:03 +0200

    Added CappedLinkedList

---
+++ src/main/java/io/github/thebusybiscuit/cscorelib2/collections/CappedLinkedList.java
@@ -0,0 +1,50 @@
+package io.github.thebusybiscuit.cscorelib2.collections;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.stream.Stream;
+
+import lombok.Getter;
+
+public class CappedLinkedList<T> implements Iterable<T> {
+
+	@Getter
+	private int size;
+	
+	private LinkedList<T> list;
+	
+	public CappedLinkedList(int size) {
+		this.size = size;
+		this.list = new LinkedList<>();
+	}
+	
+	public void clear() {
+		list.clear();
+	}
+	
+	public void add(T obj) {
+		list.add(obj);
+		
+		if (list.size() > size) {
+			list.removeFirst();
+		}
+	}
+	
+	public int size() {
+		return list.size();
+	}
+
+	public boolean isEmpty() {
+		return list.isEmpty();
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return list.iterator();
+	}
+	
+	public Stream<T> stream() {
+		return list.stream();
+	}
+
+}
