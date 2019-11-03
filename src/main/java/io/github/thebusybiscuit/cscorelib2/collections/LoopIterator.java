5f26476863d09348008b999dda272deec6f8eb89
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-11-03 20:42:32 +0100

    Added LoopIterator

---
+++ src/main/java/io/github/thebusybiscuit/cscorelib2/collections/LoopIterator.java
@@ -0,0 +1,47 @@
+package io.github.thebusybiscuit.cscorelib2.collections;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import lombok.Getter;
+import lombok.NonNull;
+
+public class LoopIterator<T> implements Iterator<T> {
+	
+	private final Queue<T> queue;
+	private final boolean hasNext;
+	
+	public LoopIterator(@NonNull Collection<T> collection) {
+		this.hasNext = collection.isEmpty();
+		
+		if (hasNext) {
+			this.queue = new LinkedList<>(collection);
+		}
+		else {
+			this.queue = null;
+		}
+	}
+	
+	@Getter
+	private int index = 0;
+	private Iterator<T> iterator;
+
+	@Override
+	public boolean hasNext() {
+		return hasNext;
+	}
+
+	@Override
+	public T next() {
+		if (iterator == null || !iterator.hasNext()) {
+			iterator = queue.iterator();
+			index = 0;
+		}
+		
+		index++;
+		return iterator.next();
+	}
+
+}
