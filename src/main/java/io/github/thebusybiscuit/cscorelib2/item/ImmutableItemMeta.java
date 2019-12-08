f0e6c44248168044a2274f2de5a03afbaffc60d1
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-12-08 10:43:08 +0100

    Added ImmutableItemMeta

---
+++ src/main/java/io/github/thebusybiscuit/cscorelib2/item/ImmutableItemMeta.java
@@ -0,0 +1,41 @@
+package io.github.thebusybiscuit.cscorelib2.item;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import lombok.Getter;
+import lombok.NonNull;
+
+/**
+ * This is an immutable version of ItemMeta.
+ * Use this class to optimize your ItemStack#getItemMeta() calls by returning
+ * a field of this immutable copy.
+ * 
+ * @author TheBusyBiscuit
+ *
+ */
+@Getter
+public class ImmutableItemMeta {
+	
+	private final Optional<String> displayName;
+	private final Optional<List<String>> lore;
+	private final Optional<Integer> customModelData;
+	
+	private final Set<ItemFlag> itemFlags;
+	private final Map<Enchantment, Integer> enchants;
+	
+	public ImmutableItemMeta(@NonNull ItemMeta meta) {
+		this.displayName = meta.hasDisplayName() ? Optional.of(meta.getDisplayName()): Optional.empty();
+		this.lore = meta.hasLore() ? Optional.of(meta.getLore()): Optional.empty();
+		this.customModelData = meta.hasCustomModelData() ? Optional.of(meta.getCustomModelData()): Optional.empty();
+		
+		this.itemFlags = meta.getItemFlags();
+		this.enchants = meta.getEnchants();
+	}
+}
