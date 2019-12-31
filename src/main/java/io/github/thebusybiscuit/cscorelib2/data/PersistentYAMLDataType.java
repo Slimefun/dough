80f1ef2b8ac5f45f02e83318496000d728843029
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-12-31 03:07:31 +0100

    Added Persistent YAML Data Type

---
+++ src/main/java/io/github/thebusybiscuit/cscorelib2/data/PersistentYAMLDataType.java
@@ -0,0 +1,46 @@
+package io.github.thebusybiscuit.cscorelib2.data;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.persistence.PersistentDataAdapterContext;
+import org.bukkit.persistence.PersistentDataType;
+
+import lombok.Cleanup;
+
+public final class PersistentYAMLDataType implements PersistentDataType<String, FileConfiguration> {
+
+	public static final PersistentYAMLDataType CONFIG = new PersistentYAMLDataType();
+	
+	private PersistentYAMLDataType() {}
+	
+	@Override
+	public Class<String> getPrimitiveType() {
+		return String.class;
+	}
+
+	@Override
+	public Class<FileConfiguration> getComplexType() {
+		return FileConfiguration.class;
+	}
+
+	@Override
+	public String toPrimitive(FileConfiguration complex, PersistentDataAdapterContext context) {
+		return complex.saveToString();
+	}
+	
+	@Override
+	public FileConfiguration fromPrimitive(String primitive, PersistentDataAdapterContext context) {
+		try {
+			@Cleanup
+			Reader reader = new StringReader(primitive);
+			return YamlConfiguration.loadConfiguration(reader);
+		} catch (IOException e) {
+			return null;
+		}
+	}
+	
+}
