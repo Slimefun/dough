123b2d09a9768203aa6a1e6e36651bbb52613321
Angeschossen <bytesoft.public@gmail.com>
2019-12-31 22:45:31 +0100

    Added Lands Integration

--- src/main/java/io/github/thebusybiscuit/cscorelib2/protection/ProtectionManager.java
+++ src/main/java/io/github/thebusybiscuit/cscorelib2/protection/ProtectionManager.java
@@ -26,6 +26,7 @@ import io.github.thebusybiscuit.cscorelib2.protection.modules.PreciousStonesProt
 import io.github.thebusybiscuit.cscorelib2.protection.modules.RedProtectProtectionModule;
 import io.github.thebusybiscuit.cscorelib2.protection.modules.TownyProtectionModule;
 import io.github.thebusybiscuit.cscorelib2.protection.modules.WorldGuardProtectionModule;
+import io.github.thebusybiscuit.cscorelib2.protection.modules.LandsProtectionModule;
 import lombok.NonNull;
 
 public final class ProtectionManager {
@@ -80,6 +81,9 @@ public final class ProtectionManager {
 		}
 		if (server.getPluginManager().isPluginEnabled("BlockLocker")) {
 			registerModule(new BlockLockerProtectionModule());
+		}
+		if(server.getPluginManager().isPluginEnabled("Lands")){
+			registerModule(new LandsProtectionModule());
 		}
 		
 		/*
