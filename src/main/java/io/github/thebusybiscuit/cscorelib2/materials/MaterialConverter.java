9fcc2c1c5c9ae1144dd524460c96698256fea169
TheBusyBiscuit <mrCookieSlime@gmail.com>
2021-01-11 00:35:44 +0100

    Removed deprecated classes

--- src/main/java/io/github/thebusybiscuit/cscorelib2/materials/MaterialConverter.java
+++ src/main/java/io/github/thebusybiscuit/cscorelib2/materials/MaterialConverter.java
@@ -5,6 +5,7 @@ import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 
 import org.bukkit.Material;
+import org.bukkit.Tag;
 
 import lombok.NonNull;
 
@@ -28,43 +29,19 @@ public final class MaterialConverter {
             return Optional.of(Material.WARPED_FUNGUS);
         }
 
-        return convert(log, MaterialCollections.getAllLogs()::contains, type -> type.substring(0, type.lastIndexOf('_')).replace("STRIPPED_", "") + "_SAPLING");
+        return convert(log, Tag.LOGS::isTagged, type -> type.substring(0, type.lastIndexOf('_')).replace("STRIPPED_", "") + "_SAPLING");
     }
 
     public static Optional<Material> getSaplingFromLeaves(@NonNull Material leaves) {
-        return convert(leaves, MaterialCollections.getAllLeaves()::contains, type -> type.replace("_LEAVES", "_SAPLING"));
+        return convert(leaves, Tag.LEAVES::isTagged, type -> type.replace("_LEAVES", "_SAPLING"));
     }
 
     public static Optional<Material> getPlanksFromLog(@NonNull Material log) {
-        return convert(log, MaterialCollections.getAllLogs()::contains, type -> type.substring(0, type.lastIndexOf('_')).replace("STRIPPED_", "") + "_PLANKS");
+        return convert(log, Tag.LOGS::isTagged, type -> type.substring(0, type.lastIndexOf('_')).replace("STRIPPED_", "") + "_PLANKS");
     }
 
     public static Optional<Material> getStrippedFromLog(@NonNull Material log) {
-        return convert(log, type -> MaterialCollections.getAllLogs().contains(type) && !type.name().startsWith("STRIPPED_"), type -> "STRIPPED_" + type);
-    }
-
-    public static Optional<Material> getGlassFromPane(@NonNull Material pane) {
-        return convert(pane, MaterialCollections.getAllStainedGlassPaneColors()::contains, type -> type.substring(0, type.length() - "_PANE".length()));
-    }
-
-    public static Optional<Material> getPaneFromGlass(@NonNull Material glass) {
-        return convert(glass, MaterialCollections.getAllStainedGlassColors()::contains, type -> type + "_PANE");
-    }
-
-    public static Optional<Material> getWoolFromDye(@NonNull Material dye) {
-        return convert(dye, MaterialCollections.getAllDyeColors()::contains, type -> type.replace("_DYE", "_WOOL"));
-    }
-
-    public static Optional<Material> getCarpetFromDye(@NonNull Material dye) {
-        return convert(dye, MaterialCollections.getAllDyeColors()::contains, type -> type.replace("_DYE", "_CARPET"));
-    }
-
-    public static Optional<Material> getStainedGlassFromDye(@NonNull Material dye) {
-        return convert(dye, MaterialCollections.getAllDyeColors()::contains, type -> type.replace("_DYE", "_STAINED_GLASS"));
-    }
-
-    public static Optional<Material> getStainedGlassPaneFromDye(@NonNull Material dye) {
-        return convert(dye, MaterialCollections.getAllDyeColors()::contains, type -> type.replace("_DYE", "_STAINED_GLASS_PANE"));
+        return convert(log, type -> Tag.LOGS.isTagged(type) && !type.name().startsWith("STRIPPED_"), type -> "STRIPPED_" + type);
     }
 
     private static Optional<Material> convert(Material type, Predicate<Material> predicate, UnaryOperator<String> converter) {
