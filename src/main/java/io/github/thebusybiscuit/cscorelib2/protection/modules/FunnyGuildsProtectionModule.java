fd8fa32ff444d40bd1e16c7eb56044d12cad4e64
barpec12 <kontakt@barpec12.pl>
2021-04-08 15:50:33 +0200

    Improve code quality

--- src/main/java/io/github/thebusybiscuit/cscorelib2/protection/modules/FunnyGuildsProtectionModule.java
+++ src/main/java/io/github/thebusybiscuit/cscorelib2/protection/modules/FunnyGuildsProtectionModule.java
@@ -2,22 +2,23 @@ package io.github.thebusybiscuit.cscorelib2.protection.modules;
 
 import io.github.thebusybiscuit.cscorelib2.protection.ProtectableAction;
 import io.github.thebusybiscuit.cscorelib2.protection.ProtectionModule;
-import me.angeschossen.lands.api.integration.LandsIntegration;
-import me.angeschossen.lands.api.land.Area;
-import me.angeschossen.lands.api.land.LandWorld;
-import me.angeschossen.lands.api.role.enums.RoleSetting;
-import net.dzikoysk.funnyguilds.FunnyGuilds;
+import lombok.NonNull;
 import net.dzikoysk.funnyguilds.system.protection.ProtectionSystem;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
+/**
+ * Provides protection handling for FunnyGuilds
+ *
+ * @author barpec12 on 05.04.2021
+ */
 public class FunnyGuildsProtectionModule implements ProtectionModule {
 
 	private final Plugin plugin;
 
-	public FunnyGuildsProtectionModule(Plugin plugin) {
+	public FunnyGuildsProtectionModule(@NonNull Plugin plugin) {
 		this.plugin = plugin;
 	}
 
@@ -37,12 +38,7 @@ public class FunnyGuildsProtectionModule implements ProtectionModule {
 	}
 
 	private boolean convert(ProtectableAction protectableAction) {
-		switch (protectableAction) {
-			case PLACE_BLOCK:
-				return true;
-			default:
-				return false;
-		}
+		return protectableAction == ProtectableAction.PLACE_BLOCK;
 	}
 
 }
