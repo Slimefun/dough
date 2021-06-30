58cc58956cd63d3e5d4fe9fcee91d9926ce63ede
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2021-06-30 13:15:17 +0200

    Enforce nullability in dough-updater

--- dough-updater/src/main/java/io/github/thebusybiscuit/dough/updater/UpdateInfo.java
+++ dough-updater/src/main/java/io/github/thebusybiscuit/dough/updater/UpdateInfo.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 import io.github.thebusybiscuit.dough.versions.Version;
+import org.apache.commons.lang.Validate;
 
 // TODO: Convert to Java 16 record
 class UpdateInfo {
@@ -14,6 +15,9 @@ class UpdateInfo {
 
     @ParametersAreNonnullByDefault
     UpdateInfo(URL url, Version version) {
+        Validate.notNull(url, "The URL cannot be null");
+        Validate.notNull(version, "The version cannot be null");
+
         this.url = url;
         this.version = version;
     }
