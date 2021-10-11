c5545f29220defb657e1ba6d4a62628d902caf82
Sefiraat <miakis@me.com>
2021-10-11 20:56:01 +0100

    set, has and getCustom methods

--- dough-data/src/main/java/io/github/bakedlibs/dough/data/persistent/PersistentDataAPI.java
+++ dough-data/src/main/java/io/github/bakedlibs/dough/data/persistent/PersistentDataAPI.java
@@ -106,6 +106,22 @@ public final class PersistentDataAPI {
         holder.getPersistentDataContainer().set(key, PersistentUUIDDataType.TYPE, uuid);
     }
 
+    /**
+     * Set a custom object based on the given {@link PersistentDataType} in a {@link PersistentDataContainer}
+     *
+     * @param holder
+     *            The {@link PersistentDataHolder} to add the data to
+     * @param key
+     *            The key of the data to set
+     * @param type
+     *            The {@link PersistentDataType} to be used.
+     * @param obj
+     *            The object to put in the container
+     */
+    public static <T, Z> void setCustom(@Nonnull PersistentDataHolder holder, @Nonnull NamespacedKey key, @Nonnull PersistentDataType<T, Z> type, @Nonnull Z obj) {
+        holder.getPersistentDataContainer().set(key, type, obj);
+    }
+
     /////////////////////////////////////
     // Has
     /////////////////////////////////////
@@ -314,6 +330,20 @@ public final class PersistentDataAPI {
      */
     public static boolean hasUUID(@Nonnull PersistentDataHolder holder, @Nonnull NamespacedKey key) {
         return holder.getPersistentDataContainer().has(key, PersistentUUIDDataType.TYPE);
+    }
+
+    /**
+     * Checks if the specified {@link PersistentDataHolder} has a {@link PersistentDataContainer} with the specified
+     * key.
+     *
+     * @param holder
+     *            The {@link PersistentDataHolder} to check
+     * @param key
+     *            The key to check for
+     * @return {@code true} if the holder has a {@link PersistentDataContainer} with the specified key.
+     */
+    public static <T, Z> boolean hasCustom(@Nonnull PersistentDataHolder holder, @Nonnull NamespacedKey key, @Nonnull PersistentDataType<T, Z> type) {
+        return holder.getPersistentDataContainer().has(key, type);
     }
 
     /////////////////////////////////////
@@ -953,6 +983,53 @@ public final class PersistentDataAPI {
     @Nonnull
     public static Optional<UUID> getOptionalUUID(@Nonnull PersistentDataHolder holder, @Nonnull NamespacedKey key) {
         return Optional.ofNullable(getUUID(holder, key));
+    }
+
+    /**
+     * Get an object based on the provided {@link PersistentDataType} in a {@link PersistentDataContainer}, if the key doesn't exist it returns null.
+     *
+     * @param holder
+     *            The {@link PersistentDataHolder} to retrieve the data from
+     * @param key
+     *            The key of the data to retrieve
+     * @return An object associated with this key or null if it doesn't exist
+     */
+    @Nullable
+    public static <T, Z> Z getCustom(@Nonnull PersistentDataHolder holder, @Nonnull NamespacedKey key, @Nonnull PersistentDataType<T, Z> type) {
+        return holder.getPersistentDataContainer().get(key, type);
+    }
+
+    /**
+     * This method returns an {@link Optional} describing the object defined by the {@link PersistentDataType}
+     * found under the given key. An empty {@link Optional} will be returned if no value has been found.
+     *
+     * @see PersistentDataAPI#getCustom(PersistentDataHolder, NamespacedKey, PersistentDataType)
+     *
+     * @param holder
+     *            The {@link PersistentDataHolder} to retrieve the data from
+     * @param key
+     *            The key of the data to retrieve
+     * @return An {@link Optional} describing the result
+     */
+    @Nonnull
+    public static <T, Z> Optional<Z> getOptionalCustom(@Nonnull PersistentDataHolder holder, @Nonnull NamespacedKey key, @Nonnull PersistentDataType<T, Z> type) {
+        return Optional.ofNullable(getCustom(holder, key, type));
+    }
+
+    /**
+     * Get an object based on the provided {@link PersistentDataType} in a {@link PersistentDataContainer} or the default value if the key doesn't exist.
+     *
+     * @param holder
+     *            The {@link PersistentDataHolder} to retrieve the data from
+     * @param key
+     *            The key of the data to retrieve
+     * @param defaultVal
+     *            The default value to use if no key is found
+     * @return The object associated with this key or the default value if it doesn't exist
+     */
+    @Nonnull
+    public static <T, Z> Z getCustom(@Nonnull PersistentDataHolder holder, @Nonnull NamespacedKey key, @Nonnull PersistentDataType<T, Z> type, @Nonnull Z defaultVal) {
+        return holder.getPersistentDataContainer().getOrDefault(key, type, defaultVal);
     }
 
     /**
