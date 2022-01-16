edb70b35bd8e470c436a84b12ab49ed9ada9bd95
TheBusyBiscuit <mrCookieSlime@gmail.com>
2022-01-16 14:35:47 +0100

    Some refactoring for `dough-protection`

--- dough-protection/src/main/java/io/github/bakedlibs/dough/protection/ProtectionManager.java
+++ dough-protection/src/main/java/io/github/bakedlibs/dough/protection/ProtectionManager.java
@@ -7,12 +7,13 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
 
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.Server;
 import org.bukkit.block.Block;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 
 import io.github.bakedlibs.dough.common.DoughLogger;
 import io.github.bakedlibs.dough.protection.loggers.CoreProtectLogger;
@@ -23,6 +24,7 @@ import io.github.bakedlibs.dough.protection.modules.ChestProtectProtectionModule
 import io.github.bakedlibs.dough.protection.modules.FactionsUUIDProtectionModule;
 import io.github.bakedlibs.dough.protection.modules.FunnyGuildsProtectionModule;
 import io.github.bakedlibs.dough.protection.modules.GriefPreventionProtectionModule;
+import io.github.bakedlibs.dough.protection.modules.HuskTownsProtectionModule;
 import io.github.bakedlibs.dough.protection.modules.LWCProtectionModule;
 import io.github.bakedlibs.dough.protection.modules.LandsProtectionModule;
 import io.github.bakedlibs.dough.protection.modules.LocketteProtectionModule;
@@ -31,7 +33,6 @@ import io.github.bakedlibs.dough.protection.modules.PreciousStonesProtectionModu
 import io.github.bakedlibs.dough.protection.modules.RedProtectProtectionModule;
 import io.github.bakedlibs.dough.protection.modules.TownyProtectionModule;
 import io.github.bakedlibs.dough.protection.modules.WorldGuardProtectionModule;
-import io.github.bakedlibs.dough.protection.modules.HuskTownsProtectionModule;
 
 /**
  * This Class provides a nifty API for plugins to query popular protection plugins.
@@ -54,41 +55,44 @@ public final class ProtectionManager {
      * @param plugin
      *            The plugin that uses dough.
      */
-    @SuppressWarnings("java:S1612")
-    public ProtectionManager(@Nonnull Server server, @Nonnull Plugin plugin) {
-        logger = new DoughLogger(server, "protection");
+    public ProtectionManager(@Nonnull Plugin plugin) {
+        logger = new DoughLogger(plugin.getServer(), "protection");
 
         logger.log(Level.INFO, "Loading Protection Modules...");
         logger.log(Level.INFO, "This may happen more than once.");
 
-        // We sadly cannot use ModuleName::new as this would load the class into memory prematurely
-        registerModule(server, "WorldGuard", modulePlugin -> new WorldGuardProtectionModule(modulePlugin));
-        registerModule(server, "Towny", modulePlugin -> new TownyProtectionModule(modulePlugin));
-        registerModule(server, "GriefPrevention", modulePlugin -> new GriefPreventionProtectionModule(modulePlugin));
-        registerModule(server, "LWC", modulePlugin -> new LWCProtectionModule(modulePlugin));
-        registerModule(server, "PreciousStones", modulePlugin -> new PreciousStonesProtectionModule(modulePlugin));
-        registerModule(server, "Lockette", modulePlugin -> new LocketteProtectionModule(modulePlugin));
-
-        registerModule(server, "RedProtect", modulePlugin -> new RedProtectProtectionModule(modulePlugin));
-        registerModule(server, "BentoBox", modulePlugin -> new BentoBoxProtectionModule(modulePlugin));
-        registerModule(server, "BlockLocker", modulePlugin -> new BlockLockerProtectionModule(modulePlugin));
-        registerModule(server, "Lands", modulePlugin -> new LandsProtectionModule(modulePlugin, plugin));
-        registerModule(server, "ChestProtect", modulePlugin -> new ChestProtectProtectionModule(modulePlugin));
-        registerModule(server, "Factions", modulePlugin -> new FactionsUUIDProtectionModule(modulePlugin));
-        registerModule(server, "FunnyGuilds", modulePlugin -> new FunnyGuildsProtectionModule(modulePlugin));
-        registerModule(server, "PlotSquared", modulePlugin -> new PlotSquaredProtectionModule(modulePlugin));
-        registerModule(server, "HuskTowns", modulePlugin -> new HuskTownsProtectionModule(modulePlugin));
+        /*
+         * The following plugins are protection plugins.
+         */
+        loadModuleImplementations(plugin);
 
         /*
-         * The following Plugins are logging plugins, not protection plugins
+         * The following Plugins are logging plugins, not protection plugins.
          */
+        loadLoggerImplementations(plugin);
+    }
 
-        if (server.getPluginManager().isPluginEnabled("CoreProtect")) {
-            registerLogger(new CoreProtectLogger());
-        }
-        if (server.getPluginManager().isPluginEnabled("LogBlock")) {
-            registerLogger(new LogBlockLogger());
-        }
+    @ParametersAreNonnullByDefault
+    @SuppressWarnings("java:S1612")
+    private void loadModuleImplementations(Plugin plugin) {
+        PluginManager pm = plugin.getServer().getPluginManager();
+
+        // We sadly cannot use ModuleName::new as this would load the class into memory prematurely
+        registerModule(pm, "WorldGuard", worldGuard -> new WorldGuardProtectionModule(worldGuard));
+        registerModule(pm, "Towny", towny -> new TownyProtectionModule(towny));
+        registerModule(pm, "GriefPrevention", griefPrevention -> new GriefPreventionProtectionModule(griefPrevention));
+        registerModule(pm, "LWC", lwc -> new LWCProtectionModule(lwc));
+        registerModule(pm, "PreciousStones", preciousStones -> new PreciousStonesProtectionModule(preciousStones));
+        registerModule(pm, "Lockette", lockette -> new LocketteProtectionModule(lockette));
+        registerModule(pm, "RedProtect", redProtect -> new RedProtectProtectionModule(redProtect));
+        registerModule(pm, "BentoBox", bentoBox -> new BentoBoxProtectionModule(bentoBox));
+        registerModule(pm, "BlockLocker", blockLocker -> new BlockLockerProtectionModule(blockLocker));
+        registerModule(pm, "Lands", lands -> new LandsProtectionModule(lands, plugin));
+        registerModule(pm, "ChestProtect", chestProtect -> new ChestProtectProtectionModule(chestProtect));
+        registerModule(pm, "Factions", factions -> new FactionsUUIDProtectionModule(factions));
+        registerModule(pm, "FunnyGuilds", funnyGuilds -> new FunnyGuildsProtectionModule(funnyGuilds));
+        registerModule(pm, "PlotSquared", plotSquared -> new PlotSquaredProtectionModule(plotSquared));
+        registerModule(pm, "HuskTowns", huskTowns -> new HuskTownsProtectionModule(huskTowns));
 
         /*
          * The following Plugins work by utilising one of the above listed
@@ -96,41 +100,57 @@ public final class ProtectionManager {
          * We do not need a module for them, but let us make the server owner
          * aware that this compatibility exists.
          */
-
-        if (server.getPluginManager().isPluginEnabled("ProtectionStones")) {
-            loadModuleMSG("ProtectionStones");
+        if (pm.isPluginEnabled("ProtectionStones")) {
+            printModuleLoaded("ProtectionStones");
         }
-        if (server.getPluginManager().isPluginEnabled("uSkyblock")) {
-            loadModuleMSG("uSkyblock");
+
+        if (pm.isPluginEnabled("uSkyblock")) {
+            printModuleLoaded("uSkyblock");
         }
     }
 
-    public void registerLogger(@Nonnull String name, @Nonnull ProtectionLogger module) {
+    @ParametersAreNonnullByDefault
+    private void loadLoggerImplementations(Plugin plugin) {
+        PluginManager pm = plugin.getServer().getPluginManager();
+
+        if (pm.isPluginEnabled("CoreProtect")) {
+            registerLogger(new CoreProtectLogger());
+        }
+        if (pm.isPluginEnabled("LogBlock")) {
+            registerLogger(new LogBlockLogger());
+        }
+    }
+
+    @ParametersAreNonnullByDefault
+    public void registerLogger(String name, ProtectionLogger module) {
         protectionLoggers.add(module);
-        loadModuleMSG(name);
+        printModuleLoaded(name);
     }
 
-    public void registerModule(@Nonnull Server server, @Nonnull String pluginName, @Nonnull Function<Plugin, ProtectionModule> constructor) {
-        Plugin plugin = server.getPluginManager().getPlugin(pluginName);
+    @ParametersAreNonnullByDefault
+    public void registerModule(PluginManager pm, String pluginName, Function<Plugin, ProtectionModule> constructor) {
+        Plugin plugin = pm.getPlugin(pluginName);
 
         if (plugin != null && plugin.isEnabled()) {
             registerModule(plugin, constructor);
         }
     }
 
-    private void registerModule(@Nonnull Plugin plugin, @Nonnull Function<Plugin, ProtectionModule> constructor) {
+    @ParametersAreNonnullByDefault
+    private void registerModule(Plugin plugin, Function<Plugin, ProtectionModule> constructor) {
         try {
             ProtectionModule module = constructor.apply(plugin);
             module.load();
 
             protectionModules.add(module);
-            loadModuleMSG(module.getName() + " v" + module.getVersion());
+            printModuleLoaded(module.getName() + " v" + module.getVersion());
         } catch (Throwable x) {
             logger.log(Level.SEVERE, x, () -> "An Error occured while registering the Protection Module: \"" + plugin.getName() + "\" v" + plugin.getDescription().getVersion());
         }
     }
 
-    public void registerLogger(@Nonnull ProtectionLogger module) {
+    @ParametersAreNonnullByDefault
+    public void registerLogger(ProtectionLogger module) {
         try {
             module.load();
             registerLogger(module.getName(), module);
@@ -139,15 +159,18 @@ public final class ProtectionManager {
         }
     }
 
-    private void loadModuleMSG(String module) {
+    @ParametersAreNonnullByDefault
+    private void printModuleLoaded(String module) {
         logger.log(Level.INFO, "Loaded Protection Module \"{0}\"", module);
     }
 
-    public boolean hasPermission(@Nonnull OfflinePlayer p, @Nonnull Block b, @Nonnull Interaction action) {
+    @ParametersAreNonnullByDefault
+    public boolean hasPermission(OfflinePlayer p, Block b, Interaction action) {
         return hasPermission(p, b.getLocation(), action);
     }
 
-    public boolean hasPermission(@Nonnull OfflinePlayer p, @Nonnull Location l, @Nonnull Interaction action) {
+    @ParametersAreNonnullByDefault
+    public boolean hasPermission(OfflinePlayer p, Location l, Interaction action) {
         for (ProtectionModule module : protectionModules) {
             try {
                 if (!module.hasPermission(p, l, action)) {
@@ -163,7 +186,8 @@ public final class ProtectionManager {
         return true;
     }
 
-    public void logAction(@Nonnull OfflinePlayer p, @Nonnull Block b, @Nonnull Interaction action) {
+    @ParametersAreNonnullByDefault
+    public void logAction(OfflinePlayer p, Block b, Interaction action) {
         for (ProtectionLogger module : protectionLoggers) {
             try {
                 module.logAction(p, b, action);
