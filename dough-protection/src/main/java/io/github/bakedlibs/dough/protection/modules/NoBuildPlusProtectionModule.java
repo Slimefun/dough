5d469369adb6a9cf58a7296973210fbd25d7ed76
p1xEL_mc <rollandhuang914@gmail.com>
2026-01-15 17:38:48 +0800

    update nobuildplus api

--- dough-protection/src/main/java/io/github/bakedlibs/dough/protection/modules/NoBuildPlusProtectionModule.java
+++ dough-protection/src/main/java/io/github/bakedlibs/dough/protection/modules/NoBuildPlusProtectionModule.java
@@ -7,7 +7,7 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import p1xel.nobuildplus.Flags;
 import p1xel.nobuildplus.NoBuildPlus;
-import p1xel.nobuildplus.Storage.Worlds;
+import p1xel.nobuildplus.storage.Worlds;
 
 public class NoBuildPlusProtectionModule implements ProtectionModule {
 
@@ -36,16 +36,16 @@ public class NoBuildPlusProtectionModule implements ProtectionModule {
 
         switch (action) {
             case BREAK_BLOCK:
-                return !plugin.getAPI().canExecute(world, Flags.destroy);
+                return !plugin.getAPI().canExecute(world, Flags.destroy, l);
             case PLACE_BLOCK:
-                return !plugin.getAPI().canExecute(world, Flags.build);
+                return !plugin.getAPI().canExecute(world, Flags.build, l);
             case ATTACK_PLAYER:
-                return !plugin.getAPI().canExecute(world, Flags.pvp);
+                return !plugin.getAPI().canExecute(world, Flags.pvp, l);
             case ATTACK_ENTITY:
-                return !plugin.getAPI().canExecute(world, Flags.mob_damage);
+                return !plugin.getAPI().canExecute(world, Flags.mob_damage, l);
             case INTERACT_ENTITY:
             case INTERACT_BLOCK:
-                return !plugin.getAPI().canExecute(world, Flags.use);
+                return !plugin.getAPI().canExecute(world, Flags.use, l);
         }
 
         return true;
